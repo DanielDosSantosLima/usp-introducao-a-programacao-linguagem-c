0105b4cbe180519cf32ee0185d60eff9760b6e73
Daniel Lima <dsl.daniel.santos.lima@gmail.com>
2025-04-21 19:35:35 -0300

    Iniciando em C

---
+++ programa1 - hello world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    // Iniciando na Linguagem C
+    printf("Hello, World!");
+}
---
+++ programa2 - exibir acentos.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main (){
+    // Programa para colocar acentos nas palavras
+    setlocale(LC_ALL,"");
+    printf("Olá, mundo!");
+}
